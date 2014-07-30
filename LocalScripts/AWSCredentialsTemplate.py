17079df2a570f7cb577f7fad1bb367223edbaf77
Kevin Coakley <kcoakley@sdsc.edu>
2014-07-30 14:30:31 -0700

    Updated AWSCredentialsTemplate.py to specify that the keyPairFile variable should contain the full path to the file

--- LocalScripts/AWSCredentialsTemplate.py
+++ LocalScripts/AWSCredentialsTemplate.py
@@ -5,7 +5,7 @@
 aws_access_key_id='Key ID'
 aws_secret_access_key='Key'
 # Get the Keypair in the EC2 Dashboard page.
-keyPairFile="<~/.ssh/KeyFilename.pem>" # name of file keeping local key
+keyPairFile="/Users/username/.ssh/KeyFilename.pem" # full path of file keeping local key
 key_name="key name" # name of keypair (not name of file where key is stored)
 # Set the security group On the EC2 page (You will need to add IP addresses if
 # you want to connect from a place previously unauthorized.
